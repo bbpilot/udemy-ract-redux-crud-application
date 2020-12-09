a5022922d13597bb25b99698299a28ca572094ce
Makoto Nakao <info@alive-website.com>
2020-12-09 19:15:26 +0900

    Create class component and functional component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 function App() {
   return (
     <React.Fragment>
       <label htmlFor="bar">bar</label>
       <input id="bar" type="text" onChange={() => { console.log('I am clicked!') }} />
+      <Cat />
     </React.Fragment>
   );
+}
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input id="bar" type="text" onChange={() => { console.log('I am clicked!') }} />
+//         <Cat />
+//       </React.Fragment>
+//     )
+//   }
+// }
+
+const Cat = () => {
+  return <div>Meow</div>
 }
 
 export default App;
